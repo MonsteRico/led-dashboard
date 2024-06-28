a60b2f2bab6f87df7e315cf66e4e833e3bbb6957
MonsteRico <boilermonster0530@yahoo.com>
2024-06-28 11:38:37 -0400

    test

--- index.ts
+++ index.ts
@@ -32,7 +32,7 @@ function delay(ms: number) {
 			inverseColors: false,
 			ledRgbSequence: "RGB",
 			limitRefreshRateHz: 0,
-			multiplexing: MuxType.P10Outdoor1R1G1BMultiplexMapper1,
+			multiplexing: MuxType.P10Outdoor1R1G1BMultiplexMapper2,
 			panelType: "",
 			parallel: 1,
 			pixelMapperConfig: "",
