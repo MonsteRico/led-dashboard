d7096e77126919fbb4d8a66e959a52c1fc153b05
MonsteRico <boilermonster0530@yahoo.com>
2024-06-28 11:37:47 -0400

    test

--- index.ts
+++ index.ts
@@ -32,7 +32,7 @@ function delay(ms: number) {
 			inverseColors: false,
 			ledRgbSequence: "RGB",
 			limitRefreshRateHz: 0,
-			multiplexing: MuxType.InversedZStripe,
+			multiplexing: MuxType.Kaler2Scan,
 			panelType: "",
 			parallel: 1,
 			pixelMapperConfig: "",
