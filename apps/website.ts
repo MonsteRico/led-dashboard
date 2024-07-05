7dd7f544b91b0330595be745f09cbb12052d257d
MonsteRico <boilermonster0530@yahoo.com>
2024-07-05 16:23:37 -0400

    websit eapp

---
+++ apps/website.ts
@@ -0,0 +1,39 @@
+import { DateTime } from "luxon";
+import type DevMatrix from "../DevMatrix";
+import App from "./app";
+import { fonts, images } from "../preload";
+import { getWeatherCodeIcon, getWeatherData, type WeatherData } from "../weather";
+import Color from "color";
+
+export default class Website extends App {
+    private buffer = new Uint8Array(this.matrix.width() * this.matrix.height() * 3);
+
+    constructor(matrix: DevMatrix) {
+        super(matrix);
+        const refreshTime = 1000 * 60 * 5; // 5 minutes
+        this.backgroundInterval = setInterval(() => this.backgroundUpdate(), refreshTime);
+    }
+
+    public update() {
+        
+    }
+
+    public onStart() {
+        console.log(this.buffer)
+    }
+
+    public backgroundUpdate() {
+        this.getBuffer();
+    }
+
+    public async initialize() {
+        this.getBuffer();
+    }
+
+    private getBuffer() {
+        // fetch from https://led-dashboard-web.vercel.app/image
+        fetch("https://led-dashboard-web.vercel.app/image")
+            .then(response => response.arrayBuffer())
+            .then(buffer => this.buffer = new Uint8Array(buffer));
+    }
+}
