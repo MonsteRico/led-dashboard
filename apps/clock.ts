eb17c28f2184fbb162c67ea308d2cbd329d3d698
MonsteRico <boilermonster0530@yahoo.com>
2024-07-10 21:10:18 -0400

    new flower

--- apps/clock.ts
+++ apps/clock.ts
@@ -13,12 +13,12 @@ export default class Clock extends App {
 
     public update() {
         this.time = DateTime.now();
-        this.matrix.drawImage(images["flowerBG.png"], 5, 0);
+        this.matrix.drawImage(images["flowerBG.png"], 0, 0);
         this.matrix.fgColor(new Color("#ffffff"));
         this.matrix.font(fonts["spleen-8x16"]);
-        this.matrix.drawText(this.time.toLocaleString(DateTime.TIME_SIMPLE), 0, 8, { leftShadow: true });
+        this.matrix.drawText(this.time.toLocaleString(DateTime.TIME_SIMPLE), 0, 8, { rightShadow: true });
         this.matrix.font(fonts["6x9"]);
         this.matrix.fgColor(new Color("#ffffff"));
-        this.matrix.drawText(this.time.toFormat("EEE LLL d"), 2, 21, { leftShadow: true });
+        this.matrix.drawText(this.time.toFormat("EEE LLL d"), 2, 21, { rightShadow: true });
     }
 }
