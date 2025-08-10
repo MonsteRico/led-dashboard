ea50bfa1322389776dad18ee2f834dc27b86dc5b
MonsteRico <boilermonster0530@yahoo.com>
2025-08-09 22:47:30 -0400

    logs

--- apps/website.ts
+++ apps/website.ts
@@ -32,10 +32,16 @@ export default class Website extends App {
     private getBuffer() {
         // fetch from https://led-dashboard-web.vercel.app/image
         fetch(`https://led-dashboard-web.vercel.app/image?key=${process.env.LED_DASHBOARD_WEB_KEY}`)
-            .then((response) => response.json())
-            .then((jsonResponse) => JSON.parse(jsonResponse))
+            .then((response) => {
+                console.log("raw response", response);
+                return response.json();
+            })
+            .then((jsonResponse) => {
+                console.log("jsonResponse", jsonResponse);
+                return JSON.parse(jsonResponse);
+            })
             .then((body: { id: string; key: string; rawBuffer: string }) => {
-                console.log(body);
+                console.log("parsed body", body);
                 const parsedBuffer = JSON.parse(body.rawBuffer) as { [key: number]: number };
                 // Convert the object back to Uint8Array
                 const values = Object.values(parsedBuffer) as number[];
